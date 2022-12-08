df8954767e61b3ab1cb6a8d39ed581e1843e22d7
barzaken <bar.zaken@boyar.org.il>
2022-12-08 11:26:17 +0200

    first backend!

---
+++ api/board/board.service.js
@@ -0,0 +1,99 @@
+const dbService = require('../../services/db.service')
+const logger = require('../../services/logger.service')
+const utilService = require('../../services/util.service')
+const ObjectId = require('mongodb').ObjectId
+
+async function query(filterBy) {
+    try {
+        // const criteria = {
+        //     vendor: { $regex: filterBy.txt, $options: 'i' }
+        // }
+        const criteria = {}
+        const collection = await dbService.getCollection('board')
+        var boards = await collection.find(criteria).toArray()
+        return boards
+    } catch (err) {
+        logger.error('cannot find boards', err)
+        throw err
+    }
+}
+
+async function getById(boardId) {
+    try {
+        const collection = await dbService.getCollection('board')
+        const board = collection.findOne({ _id: ObjectId(boardId) })
+        return board
+    } catch (err) {
+        logger.error(`while finding board ${boardId}`, err)
+        throw err
+    }
+}
+
+async function remove(boardId) {
+    try {
+        const collection = await dbService.getCollection('board')
+        await collection.deleteOne({ _id: ObjectId(boardId) })
+        return boardId
+    } catch (err) {
+        logger.error(`cannot remove board ${boardId}`, err)
+        throw err
+    }
+}
+
+async function add(board) {
+    try {
+        const collection = await dbService.getCollection('board')
+        await collection.insertOne(board)
+        return board
+    } catch (err) {
+        logger.error('cannot insert board', err)
+        throw err
+    }
+}
+
+async function update(board) {
+    console.log('board updated',board);
+    try {
+        const boardToSave = JSON.parse(JSON.stringify(board))
+        delete boardToSave._id
+        const collection = await dbService.getCollection('board')
+        await collection.updateOne({ _id: ObjectId(board._id) }, { $set: boardToSave })
+        return board
+    } catch (err) {
+        logger.error(`cannot update board ${board._id}`, err)
+        throw err
+    }
+}
+
+async function addBoardMsg(boardId, msg) {
+    try {
+        msg.id = utilService.makeId()
+        const collection = await dbService.getCollection('board')
+        await collection.updateOne({ _id: ObjectId(boardId) }, { $push: { msgs: msg } })
+        return msg
+    } catch (err) {
+        logger.error(`cannot add board msg ${boardId}`, err)
+        throw err
+    }
+}
+
+async function removeBoardMsg(boardId, msgId) {
+    try {
+        const collection = await dbService.getCollection('board')
+        await collection.updateOne({ _id: ObjectId(boardId) }, { $pull: { msgs: {id: msgId} } })
+        return msgId
+    } catch (err) {
+        logger.error(`cannot add board msg ${boardId}`, err)
+        throw err
+    }
+}
+
+module.exports = {
+    remove,
+    query,
+    getById,
+    add,
+    update,
+    addBoardMsg,
+    removeBoardMsg
+}
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  dbURL: 'mongodb+srv://bar:12@cluster0.jrdisxn.mongodb.net/?retryWrites=true&w=majority',
+  dbName : 'board_db'
+}
